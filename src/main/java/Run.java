4c6598e245dfb96f6aa28066748b83f03c7437a5
timothy <timhol@kth.se>
2018-02-20 11:06:11 +0100

    Added run

---
+++ src/main/java/Run.java
@@ -0,0 +1,16 @@
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by timothy on 2018-01-25, PACKAGE_NAME.
+ */
+public class Run {
+    public static void main(String[] args) throws IOException, SQLException {
+        //Server server = new Server(5000);
+        HttpServer httpServer = HttpServerFactory.create("http://localhost:5000/");
+        httpServer.start();
+    }
+}
